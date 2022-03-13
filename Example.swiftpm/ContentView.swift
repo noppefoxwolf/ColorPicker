4811825c699407aa727c544e5d953c4dfc740102
noppefoxwolf <noppelabs@gmail.com>
2022-03-13 21:25:11 +0900

    Update ContentView.swift

--- Example.swiftpm/ContentView.swift
+++ Example.swiftpm/ContentView.swift
@@ -3,11 +3,20 @@ import ColorPicker
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Text("ColorPicker Demo")
+            .sheet(isPresented: .constant(true)) {
+                ColorPicker()
+            }
+    }
+}
+
+
+struct ColorPicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ColorPickerViewController {
+        ColorPickerViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ColorPickerViewController, context: Context) {
+        uiViewController.selectedColor = .magenta
     }
 }
